d18f3e7cbfbe69fd0b363bb5e242038b1bdff436
Richard E. Interval III <richard.interval@gmail.com>
2016-05-13 10:27:40 -0400

    .java File & Beginnings of Functionality
    No functionality yet exists, but changes made here are moving toward
    that goal. These include the expansion of the .java file and necessary
    inclusion of the file within other areas of the Reservator code.

--- app/src/main/java/com/futurice/android/reservator/view/NavigationBar.java
+++ app/src/main/java/com/futurice/android/reservator/view/NavigationBar.java
@@ -1,7 +1,28 @@
 package com.futurice.android.reservator.view;
 
+import com.futurice.android.reservator.R;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
 /**
  * Created by Richard on 5/13/2016.
  */
-public class NavigationBar {
+public class NavigationBar extends RelativeLayout{
+    ImageButton home_button;
+    ImageButton list_button;
+    ImageButton calendar_button;
+
+    public NavigationBar(Context context) {
+        super(context);
+    }
+
+    public NavigationBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        home_button=(ImageButton) findViewById(R.id.home_button);
+        list_button=(ImageButton) findViewById(R.id.list_button);
+        calendar_button=(ImageButton) findViewById(R.id.calendar_button);
+    }
 }
